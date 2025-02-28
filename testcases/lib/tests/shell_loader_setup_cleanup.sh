cd7bb157e54587569e2f623db1f538d6a88f1041
Petr Vorel <pvorel@suse.cz>
2025-02-28 18:24:38 +0100

    shell: Add shell_loader_setup_cleanup.sh test
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

---
+++ testcases/lib/tests/shell_loader_setup_cleanup.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2025 Petr Vorel <pvorel@suse.cz>
+#
+# ---
+# env
+# {
+# }
+# ---
+
+TST_SETUP=setup
+TST_CLEANUP=cleanup
+
+setup()
+{
+	tst_res TINFO "setup executed"
+}
+
+cleanup()
+{
+	tst_res TINFO "Cleanup executed"
+}
+
+tst_test()
+{
+	tst_res TPASS "Test is executed"
+}
+
+. tst_loader.sh
