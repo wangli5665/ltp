3b1b81e662c49b04b79c2bdf807d818984a2dbd9
Li Wang <liwang@redhat.com>
2025-06-07 10:57:29 +0800

    atomic: drop the __sync builtins
    Signed-off-by: Li Wang <liwang@redhat.com>

--- include/tst_atomic.h
+++ include/tst_atomic.h
@@ -30,32 +30,6 @@ static inline void tst_atomic_store(int32_t i, tst_atomic_t *v)
 	__atomic_store_n(v, i, __ATOMIC_SEQ_CST);
 }
 
-#elif HAVE_SYNC_ADD_AND_FETCH == 1
-
-/* Use __sync built-ins (GCC >= 4.1), with explicit memory barriers. */
-
-static inline int tst_atomic_add_return(int32_t i, tst_atomic_t *v)
-{
-	return __sync_add_and_fetch(v, i);
-}
-
-static inline int32_t tst_atomic_load(tst_atomic_t *v)
-{
-	tst_atomic_t ret;
-
-	__sync_synchronize();
-	ret = *v;
-	__sync_synchronize();
-	return ret;
-}
-
-static inline void tst_atomic_store(int32_t i, tst_atomic_t *v)
-{
-	__sync_synchronize();
-	*v = i;
-	__sync_synchronize();
-}
-
 #else
 # error "Your compiler does not support atomic operations (__atomic or __sync)"
 #endif
